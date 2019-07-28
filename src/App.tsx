5d69fdb929a2d18b6298f607ca22958c3bd7208b
Ben Soutendijk <devaent1316@gmail.com>
2019-07-28 15:34:04 -0500

    copy in splash page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, createStyles } from '@material-ui/styles';
+import { Button, Theme } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  hero: {
+    textAlign: 'center',
+    flexGrow: 1,
+    backgroundImage: 'url(/assets/images/hero.jpg)',
+    backgroundPosition: '100% 0',
+    [theme.breakpoints.down('sm')]: {
+      backgroundPosition: 'center',
+    },
+    backgroundSize: 'cover',
+    width: '100%',
+    height: '540px',
+  },
+  heroHeadline: {
+    color: theme.palette.primary.contrastText,
+    marginTop: theme.spacing(12),
+  },
+  heroCaption: {
+    color: theme.palette.primary.contrastText,
+    marginTop: theme.spacing(8),
+    fontWeight: 300,
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  heroButton: {
+    marginTop: theme.spacing(4),
+  },
+  features: {
+    textAlign: 'center',
+    flexGrow: 1,
+    backgroundImage: 'url(/assets/images/CurvyLines.png)',
+    backgroundColor: '#F2E0D0',
+    height: '540px',
+    [theme.breakpoints.down('sm')]: {
+      height: '100%',
+      padding: '50px 0 50px 0',
+    },
+  },
+  featuresList: {
+    height: '100%',
+  },
+  featuresItem: {
+    width: '250px',
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <section className={classes.hero}>
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={12}>
+            <Typography className={classes.heroHeadline} color="textPrimary" variant="h3">The Ultimate Modding Tool</Typography>
+          </Grid>
+          <Grid item md={12}>
+            <Typography className={classes.heroCaption} color="textPrimary" variant="h6">Keep track of your stream's mods</Typography>
+          </Grid>
+          <Grid item md={12}>
+            <Button className={classes.heroButton} component={Link} to="/signup" color="secondary" variant="contained">Create an account</Button>
+          </Grid>
+        </Grid>
+      </section>
+      <section className={classes.features}>
+        <Grid className={classes.featuresList} container justify="space-around" alignContent="center">
+          <Grid className={classes.featuresItem} item>
+            <Grid container direction="column" spacing={4}>
+              <Grid item><i className="far fa-comments fa-4x" /></Grid>
+              <Grid item>
+                <Typography variant="h6">Get smart with your mods</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1">
+                  Keep close track of how your mods are interacting with your community. Generate reports to see which mods send the most messages, delete the most messages, and issue the most bans.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid className={classes.featuresItem} item>
+            <Grid container direction="column" spacing={4}>
+              <Grid item><i className="fas fa-hashtag fa-4x" /></Grid>
+              <Grid item>
+                <Typography variant="h6">Manage Your Social Media</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1">
+                  Find out how and when to post to your social media to maximize engagment and impressions. Add the most effective hashtags and keywords to your posts with one click.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid className={classes.featuresItem} item>
+            <Grid container direction="column" spacing={4}>
+              <Grid item><i className="far fa-chart-bar fa-4x" /></Grid>
+              <Grid item>
+                <Typography variant="h6">Report. Report. Report.</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1">
+                  Gain insights on your social media through reporting and analytics. Back up your value to potential sponsors with data and advance your career.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </section>
     </div>
   );
 }
